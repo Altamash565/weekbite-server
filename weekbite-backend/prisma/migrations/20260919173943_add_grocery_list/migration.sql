bdc44ad73651199078f24b9e9d87f9185a3b81fb
Altamash Khan <altamashidrisi9090@gmail.com>
2026-09-19 23:12:58 +0530

    feat: Implement grocerylist schema

---
+++ weekbite-backend/prisma/migrations/20260919173943_add_grocery_list/migration.sql
@@ -0,0 +1,35 @@
+-- CreateTable
+CREATE TABLE "GroceryList" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "GroceryList_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "GroceryItem" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "quantity" TEXT NOT NULL,
+    "isPurchased" BOOLEAN NOT NULL DEFAULT false,
+    "groceryListId" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "GroceryItem_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE INDEX "GroceryList_userId_idx" ON "GroceryList"("userId");
+
+-- CreateIndex
+CREATE INDEX "GroceryItem_groceryListId_idx" ON "GroceryItem"("groceryListId");
+
+-- AddForeignKey
+ALTER TABLE "GroceryList" ADD CONSTRAINT "GroceryList_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "GroceryItem" ADD CONSTRAINT "GroceryItem_groceryListId_fkey" FOREIGN KEY ("groceryListId") REFERENCES "GroceryList"("id") ON DELETE CASCADE ON UPDATE CASCADE;
